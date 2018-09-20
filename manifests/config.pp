f98c233d10d7c9e3e528eecb68bf0aaba10d99da
ronron <ant@n.io>
2018-09-20 21:19:04 +0200

    .

---
+++ manifests/config.pp
@@ -0,0 +1,17 @@
+class cyrus_imap::config (
+    $nodename = hiera('nodename'),
+    ) {
+
+    include cyrus_imap::services
+
+    file { '/etc/cyrus.conf':
+     ensure  => present,
+     content => file("cyrus_imap/${nodename}/etc/cyrus.conf"),
+     notify   => Service['cyrus-imapd'],  
+    }
+    file { '/etc/imap.conf':
+     ensure  => present,
+     content => file("cyrus_imap/${nodename}/etc/imapd.conf"),
+     notify   => Service['cyrus-imapd'],  
+    }
+}
