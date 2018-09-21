489a17f234e0bb935c018b002b97a9505c8c2ec9
ronron <ant@n.io>
2018-09-21 15:58:15 +0200

    .

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,7 @@
 class cyrus_imap::service (
 
-	$service_name = hiera('service_name')
-	$service_enable = hiera('service_enable')
+	$service_name		= hiera('service_name'),
+	$service_enable	= hiera('service_enable')
 	$service_status = hiera('service_status')
 
 	) inherits cyrus_imap {
