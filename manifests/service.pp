0054e9f8ac1439072bcb7bc93631d91e7fd83a59
ronron <ant@n.io>
2018-09-21 16:40:24 +0200

    .

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,14 +1,14 @@
 class cyrus_imap::service (
 
-	$service_namee	= hiera('service_name'),
+	$service_name		= hiera('service_name'),
 	$service_enable	= hiera('service_enable'),
 	$service_status	= hiera('service_status'),
 
 	) inherits cyrus_imap {
 
-		service { 'cyrus-imapd':
+		service { $service_name:
 			ensure  => $service_status,
 			enable  => $service_enable,
-			restart => "systemctl restart ${service_namee}",
+			restart => "systemctl restart $service_name",
 		}
 }
